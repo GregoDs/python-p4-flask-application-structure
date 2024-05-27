8e309af5368328eef56f425e509e7040d83187f0
+ <+>
2024-05-27 22:12:39 +0300

    Completed lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,20 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
+# Create an instance of the Flask class
 app = Flask(__name__)
+
+# Use app instance to register a route to handle requests to the root URL
+@app.route('/')
+def index():  # Function to handle requests to the root URL
+    return '<h1>Welcome to My Page!</h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+
+
+
+# Run the server
+if __name__ == '__main__':
+    app.run(port=5555,debug=True)
